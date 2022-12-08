ebc0ce3f0184e7115391afb255bf4b44f31a21f2
eljam3239 <elijames2301@gmail.com>
2022-12-08 15:10:24 -0500

    Part1 of day2

--- day2.py
+++ day2.py
@@ -2,8 +2,53 @@
 #right column is you: x for rock, y for paper, z for scissors
 #score = shape you selected (1/2/3 -> r/p/s) + outcome(L: 0, D: 3, W: 6)
 
+def round_score(choice1, choice2):
+    score = 0
+    if choice2=='X':
+        score+=1
+    elif choice2=='Y':
+        score+=2
+    else:
+        score+=3
+    
+    if choice1=='A' and choice2=='X':
+        score+=3
+    elif choice1=='A' and choice2 == 'Y':
+        score+=6
+    elif choice1=='A' and choice2 == 'Z':
+        score+=0
+    elif choice1 =='B' and choice2 =='X':
+        score+=0
+    elif choice1 =='B' and choice2 =='Y':
+        score+=3
+    elif choice1 == 'B' and choice2 == 'Z':
+        score+=6
+    elif choice1 == 'C' and choice2 == 'X':
+        score +=6
+    elif choice1 == 'C' and choice2 == 'Y':
+        score+=0
+    elif choice1 == 'C' and choice2 == 'Z':
+        score+=3
+    
+    return score
+
+
 total_score = 0
 
 input = open('inputday2.txt', 'r')
 
-Lines = input.readlines()
+#line = input.readline()
+#print(line)
+#choice1 = line[0]
+#choice2 = line[2]
+#print(choice1, choice2)
+
+#print(round_score(choice1, choice2))
+
+lines = input.readlines()
+for line in lines:
+    choice1 = line[0]
+    choice2 = line[2]
+    score = round_score(choice1, choice2)
+    total_score+=score
+print(total_score)
